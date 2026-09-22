1933660e973e73169d0b80bdea3eacad3dea3fe1
MK (fengmk2) <fengmk2@gmail.com>
2026-09-22 18:30:17 +0800

    test: fix browser text assertions

--- packages-private/vapor-e2e-test/__tests__/vdomInterop.spec.ts
+++ packages-private/vapor-e2e-test/__tests__/vdomInterop.spec.ts
@@ -28,54 +28,66 @@ test(
       .element(css('.vapor > h2'))
       .toHaveTextContent('This is a Vapor component in VDOM')
 
-    expect(css('.vapor-prop')).toHaveTextContent('hello')
+    expect(css('.vapor-prop')).toHaveTextContent('props.msg: hello')
 
     const l = css('.vdom-slot-in-vapor-default')
-    expect(l).toHaveTextContent('slot prop: slot prop')
-    expect(l).toHaveTextContent('component prop: hello')
+    expect(l).toHaveTextContent(
+      '#default: slot prop: slot propcomponent prop: hello',
+    )
 
     await css('.change-vdom-slot-in-vapor-prop').click()
     expect(css('.vdom-slot-in-vapor-default')).toHaveTextContent(
-      'slot prop: changed',
+      '#default: slot prop: changedcomponent prop: hello',
     )
 
-    expect(css('.vdom-slot-in-vapor-test')).toHaveTextContent('A test slot')
+    expect(css('.vdom-slot-in-vapor-test')).toHaveTextContent(
+      '#test: A test slot',
+    )
 
     await css('.toggle-vdom-slot-in-vapor').click()
     expect(css('.vdom-slot-in-vapor-test')).toHaveTextContent(
-      'fallback content',
+      '#test: fallback content',
     )
 
     await css('.toggle-vdom-slot-in-vapor').click()
-    expect(css('.vdom-slot-in-vapor-test')).toHaveTextContent('A test slot')
+    expect(css('.vdom-slot-in-vapor-test')).toHaveTextContent(
+      '#test: A test slot',
+    )
 
-    expect(css('.vdom > h2')).toHaveTextContent('VDOM component in Vapor')
+    expect(css('.vdom > h2')).toHaveTextContent(
+      'This is a VDOM component in Vapor',
+    )
 
-    expect(css('.vdom-prop')).toHaveTextContent('hello')
+    expect(css('.vdom-prop')).toHaveTextContent('props.msg: hello')
 
     const tt = css('.vapor-slot-in-vdom-default')
-    expect(tt).toHaveTextContent('slot prop: slot prop')
-    expect(tt).toHaveTextContent('component prop: hello')
+    expect(tt).toHaveTextContent(
+      '#default: slot prop: slot propcomponent prop: hello',
+    )
 
     await css('.change-vapor-slot-in-vdom-prop').click()
     expect(css('.vapor-slot-in-vdom-default')).toHaveTextContent(
-      'slot prop: changed',
+      '#default: slot prop: changedcomponent prop: hello',
     )
 
-    expect(css('.vapor-slot-in-vdom-test')).toHaveTextContent('fallback')
+    expect(css('.vapor-slot-in-vdom-test')).toHaveTextContent('#test fallback')
 
     await css('.toggle-vapor-slot-in-vdom-default').click()
     expect(css('.vapor-slot-in-vdom-default')).toHaveTextContent(
-      'default slot fallback',
+      '#default: default slot fallback',
     )
 
     await css('.toggle-vapor-slot-in-vdom-default').click()
 
     await css('input').fill('bye')
-    expect(css('.vapor-prop')).toHaveTextContent('bye')
-    expect(css('.vdom-slot-in-vapor-default')).toHaveTextContent('bye')
-    expect(css('.vdom-prop')).toHaveTextContent('bye')
-    expect(css('.vapor-slot-in-vdom-default')).toHaveTextContent('bye')
+    expect(css('.vapor-prop')).toHaveTextContent('props.msg: bye')
+    expect(css('.vdom-slot-in-vapor-default')).toHaveTextContent(
+      '#default: slot prop: changedcomponent prop: bye',
+    )
+    expect(css('.vdom-prop')).toHaveTextContent('props.msg: bye')
+    expect(css('.vapor-slot-in-vdom-default')).toHaveTextContent(
+      '#default: slot prop: changedcomponent prop: bye',
+    )
   },
   E2E_TIMEOUT,
 )
